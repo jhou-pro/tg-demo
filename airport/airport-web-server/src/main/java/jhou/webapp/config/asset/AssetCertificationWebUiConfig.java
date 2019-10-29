8e02853379f87d355fb8e6d80566b779a77bceab
jhou-pro <jhou.pro@gmail.com>
2019-10-29 14:15:16 +0200

    #4 core: registered ImportDescAction in AssetCertification master.

--- airport/airport-web-server/src/main/java/jhou/webapp/config/asset/AssetCertificationWebUiConfig.java
+++ airport/airport-web-server/src/main/java/jhou/webapp/config/asset/AssetCertificationWebUiConfig.java
@@ -1,21 +1,27 @@
 package jhou.webapp.config.asset;
 
 import static ua.com.fielden.platform.web.PrefDim.mkDim;
+import static ua.com.fielden.platform.web.centre.api.actions.impl.EntityActionBuilder.action;
+import static ua.com.fielden.platform.web.centre.api.context.impl.EntityCentreContextSelector.context;
 
 import java.util.Optional;
 
 import com.google.inject.Injector;
 
 import jhou.asset.AssetCertification;
+import jhou.asset.actions.ImportDescAction;
 import jhou.asset.producers.AssetCertificationProducer;
 import jhou.common.LayoutComposer;
 import ua.com.fielden.platform.web.PrefDim.Unit;
 import ua.com.fielden.platform.web.app.config.IWebUiBuilder;
+import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
 import ua.com.fielden.platform.web.interfaces.ILayout.Device;
+import ua.com.fielden.platform.web.minijs.JsCode;
 import ua.com.fielden.platform.web.view.master.EntityMaster;
 import ua.com.fielden.platform.web.view.master.api.IMaster;
 import ua.com.fielden.platform.web.view.master.api.actions.MasterActions;
 import ua.com.fielden.platform.web.view.master.api.impl.SimpleMasterBuilder;
+
 /**
  * {@link AssetCertification} Web UI configuration.
  *
@@ -43,11 +49,19 @@ public class AssetCertificationWebUiConfig {
      */
     private EntityMaster<AssetCertification> createMaster(final Injector injector) {
         final String layout = LayoutComposer.mkGridForMasterFitWidth(3, 1);
-
+        
+        final EntityActionConfig importDescAction = action(ImportDescAction.class)
+                .withContext(context().withMasterEntity().build())
+                .postActionSuccess(() -> new JsCode("self.setEditorValue4Property('desc', functionalEntity, 'desc');"))
+                .icon("icons:input")
+                .longDesc("Import Description from related Asset")
+                .withNoParentCentreRefresh()
+                .build();
+        
         final IMaster<AssetCertification> masterConfig = new SimpleMasterBuilder<AssetCertification>().forEntity(AssetCertification.class)
                 .addProp("asset").asAutocompleter().also()
                 .addProp("certification").asAutocompleter().also()
-                .addProp("desc").asMultilineText().also()
+                .addProp("desc").asMultilineText().withAction(importDescAction).also()
                 .addAction(MasterActions.REFRESH).shortDesc("Cancel").longDesc("Cancel action")
                 .addAction(MasterActions.SAVE)
                 .setActionBarLayoutFor(Device.DESKTOP, Optional.empty(), LayoutComposer.mkActionLayoutForMaster())
