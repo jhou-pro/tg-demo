18d0f169bfa98acf6d7fbb106b67890f21fc87fb
jhou-pro <jhou.pro@gmail.com>
2019-10-29 16:29:43 +0200

    #6 core: converted OpenRelatedAssetsAction to functional form with no key.

--- airport/airport-pojo-bl/src/main/java/jhou/asset/actions/OpenRelatedAssetsAction.java
+++ airport/airport-pojo-bl/src/main/java/jhou/asset/actions/OpenRelatedAssetsAction.java
@@ -1,9 +1,11 @@
 package jhou.asset.actions;
 
-import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.entity.annotation.KeyType;
-import ua.com.fielden.platform.entity.annotation.KeyTitle;
+import static ua.com.fielden.platform.entity.NoKey.NO_KEY;
+
+import ua.com.fielden.platform.entity.AbstractFunctionalEntityWithCentreContext;
+import ua.com.fielden.platform.entity.NoKey;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
+import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.utils.Pair;
 
@@ -13,13 +15,16 @@ import ua.com.fielden.platform.utils.Pair;
  * @author Developers
  *
  */
-@KeyType(String.class)
-@KeyTitle("Key")
+@KeyType(NoKey.class)
 @CompanionObject(IOpenRelatedAssetsAction.class)
-public class OpenRelatedAssetsAction extends AbstractEntity<String> {
+public class OpenRelatedAssetsAction extends AbstractFunctionalEntityWithCentreContext<NoKey> {
 
     private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(OpenRelatedAssetsAction.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
-
+    
+    protected OpenRelatedAssetsAction() {
+        setKey(NO_KEY);
+    }
+    
 }
