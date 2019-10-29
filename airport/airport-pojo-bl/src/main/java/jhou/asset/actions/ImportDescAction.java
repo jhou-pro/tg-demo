b8008fd864f7d7ec0f628e84a00eb37041166be5
jhou-pro <jhou.pro@gmail.com>
2019-10-29 13:28:06 +0200

    #4 core: converted ImportDescAction to functional entity with no key and added description property to it (aka added @DescTitle annotation).

--- airport/airport-pojo-bl/src/main/java/jhou/asset/actions/ImportDescAction.java
+++ airport/airport-pojo-bl/src/main/java/jhou/asset/actions/ImportDescAction.java
@@ -1,9 +1,12 @@
 package jhou.asset.actions;
 
-import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.entity.annotation.KeyType;
-import ua.com.fielden.platform.entity.annotation.KeyTitle;
+import static ua.com.fielden.platform.entity.NoKey.NO_KEY;
+
+import ua.com.fielden.platform.entity.AbstractFunctionalEntityWithCentreContext;
+import ua.com.fielden.platform.entity.NoKey;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
+import ua.com.fielden.platform.entity.annotation.DescTitle;
+import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.utils.Pair;
 
@@ -13,13 +16,17 @@ import ua.com.fielden.platform.utils.Pair;
  * @author Developers
  *
  */
-@KeyType(String.class)
-@KeyTitle("Key")
+@KeyType(NoKey.class)
 @CompanionObject(IImportDescAction.class)
-public class ImportDescAction extends AbstractEntity<String> {
+@DescTitle(value = "Description", desc = "Collected Desc from related Asset")
+public class ImportDescAction extends AbstractFunctionalEntityWithCentreContext<NoKey> {
 
     private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(ImportDescAction.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
-
+    
+    protected ImportDescAction() {
+        setKey(NO_KEY);
+    }
+    
 }
